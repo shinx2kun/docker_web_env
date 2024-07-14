81183a412f0edbe79eb7fed9cec1a933b0630240
shinx2kun <shinshin@shinshinnoMacBook-Pro.local>
2024-07-14 13:17:40 +0900

    create mysql

--- backend/web_back/config/settings.py
+++ backend/web_back/config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-+l=xp3d#ty=d@l8(+l+*olqb=6kn48$$i6v5!q9op*lv7a%_qo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'procedureManual',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
